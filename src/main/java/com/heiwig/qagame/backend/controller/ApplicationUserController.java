f56e9e5ec972502f445236cd86c3312f2cc1c096
Florian Heiwig <florian.hiwg@gmail.com>
2019-06-15 14:29:38 +0200

    refactored the name of the User entity to ApplicationUser

---
+++ src/main/java/com/heiwig/qagame/backend/controller/ApplicationUserController.java
@@ -0,0 +1,27 @@
+package com.heiwig.qagame.backend.controller;
+
+import com.heiwig.qagame.backend.entity.ApplicationUser;
+import com.heiwig.qagame.backend.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class ApplicationUserController {
+
+    @Autowired
+    private ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody ApplicationUser applicationUser) {
+        applicationUser.setPassword(bCryptPasswordEncoder.encode(applicationUser.getPassword()));
+        applicationUserRepository.save(applicationUser);
+    }
+}
---
+++ src/main/java/com/heiwig/qagame/backend/repository/ApplicationUserRepository.java
@@ -0,0 +1,10 @@
+package com.heiwig.qagame.backend.repository;
+
+import com.heiwig.qagame.backend.entity.ApplicationUser;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationUserRepository extends MongoRepository<ApplicationUser, String> {
+    ApplicationUser findByUsername(String username);
+}
