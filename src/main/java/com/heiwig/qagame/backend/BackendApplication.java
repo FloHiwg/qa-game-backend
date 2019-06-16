39a7c17ee050a61ae7d1e1d0b736b6fb60ebcd87
Florian Heiwig <florian.hiwg@gmail.com>
2019-06-16 16:17:06 +0200

    Changed swagger version to version 3.0 snapshot to get it working with spring data

--- src/main/java/com/heiwig/qagame/backend/BackendApplication.java
+++ src/main/java/com/heiwig/qagame/backend/BackendApplication.java
@@ -3,9 +3,14 @@ package com.heiwig.qagame.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class BackendApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
